f3d5b0e13a2a072dd7e60e71655396a372c8592a
István Antal <istvan.m.antal@gmail.com>
2016-06-05 14:05:31 +0100

    Added basic test

---
+++ app.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+
+pin_number=7
+
+GPIO.setup(pin_number, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def motion_sense(channel):
+    if GPIO.input(pin_number):
+        print "Motion detected"
+
+GPIO.add_event_detect(pin_number, GPIO.BOTH, callback=motion_sense, bouncetime=300)
