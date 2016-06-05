24c2006e69bc8b3366acb6a46d6546fd0b9faf0a
István Antal <istvan.m.antal@gmail.com>
2016-06-05 14:18:13 +0100

    Added timestamp to log

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
+import datetime
 GPIO.setmode(GPIO.BOARD)
 
 pin_number=7
 
 GPIO.setup(pin_number, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-def motion_sense(pir_pin):
-    print "Motion detected: " + str(pir_pin)
+def motion_sense():
+    print datetime.datetime.utcnow() +  " Motion detected"
 
 try:
     GPIO.add_event_detect(pin_number, GPIO.RISING, callback=motion_sense)
